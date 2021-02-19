9a4516a937eefd0c8e29609f6807e9dece06c468
Bryan Burns <bryanburns93@gmail.com>
2021-02-19 11:31:36 -0500

    finish backend auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,14 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
 
+router.post('/test', function (req, res) {
+	res.json({ requestBody: req.body })
+})
+
 router.get('/hello/world', function (req, res) {
 	res.cookie('XSRF-TOKEN', req.csrfToken())
-	// console.log(req.headers.cookie.split(';').forEach(x => x.split('=')))
-	res.send(`Hello World! token: ${res}`)
+	const cookies = req.headers.cookie.split(';').map(x => x.split('='))
+	res.send(`Hello World!<ul style="background-color:#ff88ff; border-radius:5px; padding: 25px; overflow:wrap">${cookies.map(cookie => `<li>${cookie[0]} == <b>${cookie[1]}</b></li>`)}</ul>`)
 })
 
 module.exports = router
