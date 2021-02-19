b84fd92f89eb2acb6b4257c7d38953647a8c8343
Bryan Burns <bryanburns93@gmail.com>
2021-02-19 15:11:31 -0500

    heroku setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,18 +4,14 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
 
-router.get('/hello/world', function (req, res) {
-	res.cookie('XSRF-TOKEN', req.csrfToken())
-	const cookies = req.headers.cookie.split(';').map(x => x.split('='))
-	res.send(`Hello World!<ul style="background-color:#ff88ff; border-radius:5px; padding: 25px; overflow:wrap">${cookies.map(cookie => `<li>${cookie[0]} == <b>${cookie[1]}</b></li>`)}</ul>`)
-})
-
+// Static routes
+// Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	// Serve the frontend's index.html file at the root route
 	router.get('/', (req, res) => {
 		res.cookie('XSRF-TOKEN', req.csrfToken())
-		return res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
 	})
 
 	// Serve the static assets in the frontend's build folder
@@ -24,14 +20,15 @@ if (process.env.NODE_ENV === 'production') {
 	// Serve the frontend's index.html file at all other routes NOT starting with /api
 	router.get(/^(?!\/?api).*/, (req, res) => {
 		res.cookie('XSRF-TOKEN', req.csrfToken())
-		return res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
 	})
 }
 
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
 	router.get('/api/csrf/restore', (req, res) => {
 		res.cookie('XSRF-TOKEN', req.csrfToken())
-		return res.json({})
+		res.status(201).json({})
 	})
 }
 
